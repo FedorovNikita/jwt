fe418b069624b8719480309d29ae21a5853d1010
Nikita Fedorov <fedorovni1996@yandex.ru>
2022-04-05 23:54:17 +0500

    moves change auth in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
-import { useSelector } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
-import Layout from './components/Layout';
-import RequireAuth from './components/RequireAuth';
-import Login from './pages/login';
-import Rules from './pages/Rules';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import Layout from "./components/Layout";
+import RequireAuth from "./components/RequireAuth";
+import { getTokens } from "./helpers/setAndRemoveToken";
+import Login from "./pages/login";
+import Rules from "./pages/Rules";
+import { checkAuth } from "./redux/actions/fetchActions";
 
 function App() {
+  const { isAuth } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
+  useEffect(() => {
+    const { refreshToken } = getTokens();
+    if (refreshToken) {
+      dispatch(checkAuth());
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isAuth) {
+      navigate('/rules', { replace: true });
+    }
+  }, [isAuth, navigate, from]);
+
   return (
     <Routes>
       {/* public routes */}
-      <Route path="login" element={<Login />} />
-      <Route path="/" element={<Layout />} >
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Layout />}>
         {/* protect routes */}
         <Route element={<RequireAuth />}>
           <Route path="/rules" element={<Rules />} />
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,31 +1,13 @@
 import { useState } from "react";
-import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useLocation, useNavigate } from "react-router-dom";
-import { getTokens } from "../helpers/setAndRemoveToken";
-import { checkAuth, login } from "../redux/actions/fetchActions";
+import { login } from "../redux/actions/fetchActions";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const {isAuth, isLoading} = useSelector((state) => state.auth);
+  const {isLoading} = useSelector((state) => state.auth);
   const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const location = useLocation();
-  const from = location.state?.from?.pathname || "/";
 
-  useEffect(() => {
-    const { refreshToken } = getTokens();
-    if (refreshToken) {
-      dispatch(checkAuth());
-    }
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (isAuth) {
-      navigate(from, { replace: true });
-    }
-  }, [isAuth, navigate, from]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
